4348b2acf4545c2dd5c6103e9d054688f35b622c
CiciliaDwi <167595039+CiciliaDwi@users.noreply.github.com>
2024-12-21 01:47:45 +0700

    semogaberhasil

---
+++ v/pesan.php
@@ -0,0 +1,243 @@
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Chat</title>
+    <link rel="stylesheet" href="css/pesan.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <div class="container-fluid h-100">
+        <div class="row justify-content-center h-100">
+            <div class="col-md-4 col-xl-3 chat">
+                <div class="card mb-sm-3 mb-md-0 contacts_card">
+                    <div class="card-header">
+                        <div class="input-group">
+                            <input type="text" placeholder="Search..." name="" class="form-control search">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text search_btn"><i class="fas fa-search"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body contacts_body">
+                        <ui class="contacts">
+                            <li class="active">
+                                <div class="d-flex bd-highlight">
+                                    <div class="img_cont">
+                                        <img src="image/cil.jpg" class="rounded-circle user_img">
+                                        <span class="online_icon"></span>
+                                    </div>
+                                    <div class="user_info">
+                                        <span>Cicil</span>
+                                        <p>Cicil is online</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex bd-highlight">
+                                    <div class="img_cont">
+                                        <img src="image/celin.jpg" class="rounded-circle user_img">
+                                        <span class="online_icon offline"></span>
+                                    </div>
+                                    <div class="user_info">
+                                        <span>Ce Erlin</span>
+                                        <p>Ce Erlin left 7 mins ago</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex bd-highlight">
+                                    <div class="img_cont">
+                                        <img src="image/sy46.jpg" class="rounded-circle user_img">
+                                    </div>
+                                    <div class="user_info">
+                                        <span>SY 46</span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex bd-highlight">
+                                    <div class="img_cont">
+                                        <img src="image/kodav.jpg" class="rounded-circle user_img">
+                                        <span class="online_icon"></span>
+                                    </div>
+                                    <div class="user_info">
+                                        <span>Ko Dav</span>
+                                        <p>Ko Dav is online</p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="d-flex bd-highlight">
+                                    <div class="img_cont">
+                                        <img src="image/flo.jpg" class="rounded-circle user_img">
+                                        <span class="online_icon"></span>
+                                    </div>
+                                    <div class="user_info">
+                                        <span>Jeflo</span>
+                                        <p>Jeflo is online</p>
+                                    </div>
+                                </div>
+                            </li>
+
+                        </ui>
+                    </div>
+                    <div class="card-footer"></div>
+                </div>
+            </div>
+            <div class="col-md-8 col-xl-6 chat">
+                <div class="card">
+                    <div class="card-header msg_head">
+                        <div class="d-flex bd-highlight">
+                            <div class="img_cont">
+                                <img src="image/cil.jpg" class="rounded-circle user_img">
+                                <span class="online_icon"></span>
+                            </div>
+                            <div class="user_info">
+                                <span>Cicil Baik Hati</span>
+                            </div>
+                            <div class="video_cam">
+                                <span><i class="fas fa-video"></i></span>
+                                <span><i class="fas fa-phone"></i></span>
+                            </div>
+                        </div>
+                        <span id="action_menu_btn"><i class="fas fa-ellipsis-v"></i></span>
+                    </div>
+                    <div class="card-body msg_card_body">
+                        <div class="d-flex justify-content-start mb-4">
+                            <div class="img_cont_msg">
+                                <img src="image/cil.jpg" class="rounded-circle user_img_msg">
+                            </div>
+                            <div class="msg_cotainer">P</div>
+                        </div>
+                        <div class="d-flex justify-content-end mb-4">
+                            <div class="msg_cotainer_send">
+                                Ha apa
+                            </div>
+                            <div class="img_cont_msg">
+                                <img src="image/v.jpg" class="rounded-circle user_img_msg">
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-start mb-4">
+                            <div class="img_cont_msg">
+                                <img src="image/cil.jpg" class="rounded-circle user_img_msg">
+                            </div>
+                            <div class="msg_cotainer">
+                                Merry Christmas Stevie Thio🥳🥳
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-end mb-4">
+                            <div class="msg_cotainer_send">
+                                Merry Christmas juga cil!
+                            </div>
+                            <div class="img_cont_msg">
+                                <img src="image/v.jpg" class="rounded-circle user_img_msg">
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-start mb-4">
+                            <div class="img_cont_msg">
+                                <img src="image/cil.jpg" class="rounded-circle user_img_msg">
+                            </div>
+                            <div class="msg_cotainer">
+                                Em, mau menulis sedikit surat yang kmrn gajadi ditulisa waktu kita fellowship.. Sekalian
+                                pesan tahun baru yaa...
+                                Terimakasih sudah menjalankan tahun 2024 dengan sangat luar biasa, semoga di tahun 2025
+                                Tuhan pake v lebih besar lagi, inget jangan sombong!! Smoga lancar juga hubungannya sama
+                                ko dav. Terus kita 2025 udah ada di semester akhir kan😭 semangat ya nanti kuliahnya.
+                                Terus sorry kalo di 2024 ini msih bnyk hal yang aku lakuin yg buat kamu ksel, 2025 nnti
+                                dijamin akan makin ksel lagi🥰 Thnkyou udah selalu support juga. Inget v kerja di ladang
+                                Tuhan ga mudah, jadi jangan lelah ya. Smoga ga jompo-jompo lagi yaa. Teruslah berusaha
+                                jadi vivi yang lebih baik lagi.
+                                Merry Chrismas and Happy New Year bro🥳🥳🫶🏻
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-end mb-4">
+                            <div class="msg_cotainer_send">
+                                WAH MAKASIH YA CICIL YANG BAIK HATI
+                            </div>
+                            <div class="img_cont_msg">
+                                <img src="image/v.jpg" class="rounded-circle user_img_msg">
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-start mb-4">
+                            <div class="img_cont_msg">
+                                <img src="image/cil.jpg" class="rounded-circle user_img_msg">
+                            </div>
+                            <div class="msg_cotainer">
+                                oke, sama-sama, God bless you!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="input-group">
+                            <div class="input-group-append">
+                                <span class="input-group-text attach_btn"><i class="fas fa-paperclip"></i></span>
+                            </div>
+                            <textarea name="" class="form-control type_msg"
+                                placeholder="Type your message..."></textarea>
+                            <div class="input-group-append">
+                                <span class="input-group-text send_btn"><i class="fas fa-location-arrow"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Menyertakan elemen audio di pesan.php -->
+    <audio id="christmasSong" autoplay loop>
+        <source src="audio/christmas2.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+
+    <!-- Tombol kontrol musik -->
+    <div style="text-align: center; margin-top: 10px;">
+        <button onclick="playAudio()" class="btn btn-primary">Play</button>
+        <button onclick="pauseAudio()" class="btn btn-primary">Pause</button>
+        <button onclick="window.history.back()" class="btn btn-primary">Back</button>
+    </div>
+
+    <!-- Script untuk kontrol audio -->
+    <script>
+        const audio = document.getElementById('christmasSong');
+
+        // Ambil posisi terakhir lagu dari localStorage saat halaman dimuat
+        window.onload = function () {
+            const savedTime = localStorage.getItem('audioCurrentTime');
+            if (savedTime) {
+                audio.currentTime = parseFloat(savedTime);
+            }
+
+            if (localStorage.getItem('audioStatus') === 'playing') {
+                audio.play();
+            } else if (localStorage.getItem('audioStatus') === 'paused') {
+                audio.pause();
+            }
+        };
+
+        // Simpan posisi lagu sebelum meninggalkan halaman
+        window.onbeforeunload = function () {
+            localStorage.setItem('audioCurrentTime', audio.currentTime);
+        };
+
+
+        function playAudio() {
+            audio.play();
+            localStorage.setItem('audioStatus', 'playing');
+        }
+
+        function pauseAudio() {
+            audio.pause();
+            localStorage.setItem('audioStatus', 'paused');
+        }
+    </script>
+</body>
+
+</html>
